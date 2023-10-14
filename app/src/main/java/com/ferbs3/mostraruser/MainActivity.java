918dc37506aca3726ebdcc8c08771e4a66b1fe2f
FerBS3 <ferblase313@gmail.com>
2023-10-13 21:39:52 -0600

    feat:obtencion de los datos y mostrarlo en pantalla

--- app/src/main/java/com/ferbs3/mostraruser/MainActivity.java
+++ app/src/main/java/com/ferbs3/mostraruser/MainActivity.java
@@ -3,12 +3,31 @@ package com.ferbs3.mostraruser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.modelos.Usuario;
 
 public class MainActivity extends AppCompatActivity {
+    Usuario usuario = new Usuario();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText pNombre = (EditText) findViewById(R.id.nombre);
+        EditText pEdad = (EditText) findViewById(R.id.edad);
+        EditText pIngresos = (EditText) findViewById(R.id.ingresos);
+        EditText pAltura = (EditText) findViewById(R.id.altura);
+        EditText pGenero = (EditText) findViewById(R.id.genero);
+
+        pNombre.setText(usuario.getNombre());
+        pEdad.setText(String.valueOf(usuario.getEdad()));
+        pIngresos.setText(String.valueOf(usuario.getIngresos()));
+        pAltura.setText(String.valueOf(usuario.getAltura()));
+        pGenero.setText(usuario.getGenero());
+
     }
 }
--- app/src/main/java/com/modelos/Usuario.java
+++ app/src/main/java/com/modelos/Usuario.java
@@ -7,11 +7,51 @@ public class Usuario {
     private int altura;
     private String genero;
 
-    public Usuario(String nombre, int edad, int ingresos, int altura, String genero) {
+    public Usuario() {
+        this.nombre = "Fernando Racista con Gonzalo";
+        this.edad = 20;
+        this.ingresos = 10;
+        this.altura = 180;
+        this.genero = "racista";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
         this.edad = edad;
+    }
+
+    public int getIngresos() {
+        return ingresos;
+    }
+
+    public void setIngresos(int ingresos) {
         this.ingresos = ingresos;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
         this.altura = altura;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
         this.genero = genero;
     }
 }
