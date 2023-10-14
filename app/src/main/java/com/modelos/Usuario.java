f42b1e31c10ccad77912199666e286061445c88f
Laboratorio Estudiantes <laboratorio@ulatina.net>
2023-10-13 21:02:13 -0600

    Commit inicial

---
+++ app/src/main/java/com/modelos/Usuario.java
@@ -0,0 +1,17 @@
+package com.modelos;
+
+public class Usuario {
+    private String nombre;
+    private int edad;
+    private int ingresos;
+    private int altura;
+    private String genero;
+
+    public Usuario(String nombre, int edad, int ingresos, int altura, String genero) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.ingresos = ingresos;
+        this.altura = altura;
+        this.genero = genero;
+    }
+}
